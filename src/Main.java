a635006606a117e7f19c3019395004d2384a326c
Rodrigo Marques <rmgmarques10@gmail.com>
2021-10-06 17:05:22 +0100

    Main output message fixed and added new methods to ContactBook.

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
     public static final String SAME_PHONE_NUMBERS = "There are contacts that share phone numbers.";
-    public static final String DIFFERENT_PHONE_NUMBERS = "All contacts have different phone numbers";
+    public static final String DIFFERENT_PHONE_NUMBERS = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -60,6 +60,7 @@ public class Main {
                     break;
                 case LOOK_UP_CONTACT:
                     getLookUpContact(in, cBook);
+                    break;
                 case CHECK_EQUAL_PHONES:
                     hasSamePhoneNumbers(cBook);
                     break;
--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -112,5 +112,16 @@ public class ContactBook {
         return contacts[currentContact++];
     }
 
+    public boolean hasSamePhoneNumber(){
+
+        for(int i = 0; i < counter; i++){
+            for(int j = i + 1; j < counter; j++){
+                if(contacts[i].getPhone() == contacts[j].getPhone())
+                    return true;
+            }
+        }
+        return false;
+
+    }
 
 }
